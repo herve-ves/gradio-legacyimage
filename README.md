f497c27e742f00c14aaa286ce9e846f83a951af8
hervelin <herve.lin@vesync.com>
2023-11-15 18:59:50 +0800

    Improve document and demo

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 # gradio-legacyimage
 A Custom Gradio component.
 
-Port the Image component in Gradio 3.X to Gradio 4.X. Keep the original APIs and functionalities.
+Port the Image component in Gradio 3.X to Gradio 4.X. Keep the original APIs and functionalities(Except the data structure of inputs and outputs, there is a slient different).
 
 ## Example usage
 
--- demo/app.py
+++ demo/app.py
@@ -6,15 +6,18 @@ from gradio_legacyimage import LegacyImage
 def process(x):
     flip = x.copy()
     flip["back"] = np.fliplr(flip["back"])
-    return x, flip
+    mask = x.copy()
+    mask["back"] = mask["mask"]
+    return x, flip, mask
 
 with gr.Blocks() as demo:
     with gr.Column():
         im1 = LegacyImage(source="upload", type="pil", tool="sketch")
         im2 = LegacyImage()
         im3 = LegacyImage()
+        im4 = LegacyImage()
 
     btn = gr.Button()
-    btn.click(process, inputs=im1, outputs=[im2, im3])
+    btn.click(process, inputs=im1, outputs=[im2, im3, im4])
 
 demo.launch()
